6f5b641c3113892aefd6715671786b5e4768ae5c
Tyler <blackbelt@yahoo.com>
2017-08-08 13:45:29 -0400

    edited project 4

--- src/project-4.js
+++ src/project-4.js
@@ -37,7 +37,15 @@ const contains = (collection, item, cb) => {
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
-
+  const newCollection = collection;
+  for (let i = 0; i < collection.length; i++) {
+    for (let j = 0; j < collection.length; j++) {
+      if (i !== j && collection[i] === collection[j]) {
+        newCollection.splice(i, 1);
+      }
+    }
+  }
+  cb(collection);
 };
 
 module.exports = {
